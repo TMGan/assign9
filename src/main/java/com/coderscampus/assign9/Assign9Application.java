edd14ec6a9d1401d78745e7e3578a779d7c94d04
TMGan <gangadeentim@gmail.com>
2025-05-28 19:43:06 -0400

    creating repo

---
+++ src/main/java/com/coderscampus/assign9/Assign9Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.assign9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assign9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assign9Application.class, args);
+	}
+
+}
